a5d0e27f9f3ae4cd53801abe26e846145b0c21ab
James Mosier <jamesdmosier@gmail.com>
2015-11-18 21:56:02 -0500

    added pause & remove on hover

--- app/js/modules/notify.js
+++ app/js/modules/notify.js
@@ -7,11 +7,21 @@ var Notify = function(color, message, title, options) {
   var bulletinElement = BuildElement(color, message, title);
 
   var duration = parseInt(options.duration);
+  var body = document.getElementsByTagName('body')[0];
 
   var waitToHide = new Timer(function() {
-    var body = document.getElementsByTagName('body')[0];
     body.removeChild(bulletinElement);
   }, duration);
+
+  bulletinElement.addEventListener('mouseover', function () {
+    console.log('mousing over');
+    waitToHide.pause();
+  }.bind(this), true);
+
+  bulletinElement.addEventListener('mouseout', function () {
+    console.log('mousing OUT');
+    waitToHide.resume();
+  }.bind(this), true);
 };
 
 export default Notify;
