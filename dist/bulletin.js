a9663e9b3e8497cea64faf9a3b52c3f5db63bf2d
James Mosier <jamesdmosier@gmail.com>
2015-11-26 20:16:51 -0500

    removed lodash dep & recompiled source

--- dist/bulletin.js
+++ dist/bulletin.js
@@ -44,16 +44,167 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(1);
+	module.exports = __webpack_require__(5);
 
 
 /***/ },
 /* 1 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var ElementCount = (function () {
+	
+	  var count = 0;
+	
+	  function getElementCount() {
+	    return count;
+	  }
+	
+	  function decreaseElementCount() {
+	    if (!count) {
+	      count = 0;
+	      return count;
+	    }
+	
+	    count = count - 1;
+	    return count;
+	  }
+	
+	  function increaseElementCount() {
+	    if (!count) {
+	      count = 0;
+	    }
+	
+	    count = count + 1;
+	    return count;
+	  }
+	
+	  return {
+	    get: getElementCount,
+	    increase: increaseElementCount,
+	    decrease: decreaseElementCount
+	  };
+	})();
+	
+	exports.default = ElementCount;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var BuildElement = function BuildElement(color, message, title) {
+	
+	  var bulletinInner = document.createElement('div');
+	  bulletinInner.className = 'bulletin-inner';
+	
+	  var titleElem = document.createElement('div');
+	  titleElem.className = 'bulletin-title';
+	
+	  var messageElem = document.createElement('div');
+	  messageElem.className = 'bulletin-message';
+	
+	  var titleText = document.createTextNode(title);
+	  var messageText = document.createTextNode(message);
+	
+	  titleElem.appendChild(titleText);
+	  messageElem.appendChild(messageText);
+	
+	  bulletinInner.appendChild(titleElem);
+	  bulletinInner.appendChild(messageElem);
+	
+	  return bulletinInner;
+	};
+	
+	exports.default = BuildElement;
+
+/***/ },
+/* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 	
-	var _Bulletin = __webpack_require__(2);
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _elementCount = __webpack_require__(1);
+	
+	var _elementCount2 = _interopRequireDefault(_elementCount);
+	
+	var _fadeOut = __webpack_require__(10);
+	
+	var _fadeOut2 = _interopRequireDefault(_fadeOut);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	var Dismiss = function Dismiss(elem, bulletinOuter) {
+	  function domRemoval() {
+	    bulletinOuter.removeChild(elem);
+	
+	    var count = _elementCount2.default.decrease();
+	    if (count === 0) {
+	      var body = document.getElementsByTagName('body')[0];
+	      body.removeChild(bulletinOuter);
+	    }
+	  }
+	
+	  (0, _fadeOut2.default)(elem, domRemoval);
+	};
+	
+	exports.default = Dismiss;
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var Timer = function Timer(callback, delay) {
+	  //http://stackoverflow.com/questions/3969475/javascript-pause-settimeout
+	
+	  var timerId,
+	      start,
+	      remaining = delay;
+	
+	  this.pause = function () {
+	    window.clearTimeout(timerId);
+	    remaining -= new Date() - start;
+	  };
+	
+	  this.resume = function () {
+	    start = new Date();
+	    window.clearTimeout(timerId);
+	    timerId = window.setTimeout(callback, remaining);
+	  };
+	
+	  this.clear = function () {
+	    window.clearTimeout(timerId);
+	  };
+	
+	  this.resume();
+	};
+	
+	exports.default = Timer;
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var _Bulletin = __webpack_require__(6);
 	
 	var _Bulletin2 = _interopRequireDefault(_Bulletin);
 	
@@ -66,7 +217,7 @@
 	})(window);
 
 /***/ },
-/* 2 */
+/* 6 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -75,15 +226,15 @@
 	  value: true
 	});
 	
-	var _notify = __webpack_require__(3);
+	var _notify = __webpack_require__(9);
 	
 	var _notify2 = _interopRequireDefault(_notify);
 	
-	var _ask = __webpack_require__(9);
+	var _ask = __webpack_require__(7);
 	
 	var _ask2 = _interopRequireDefault(_ask);
 	
-	var _elementCount = __webpack_require__(6);
+	var _elementCount = __webpack_require__(1);
 	
 	var _elementCount2 = _interopRequireDefault(_elementCount);
 	
@@ -134,7 +285,7 @@
 	exports.default = Bulletin;
 
 /***/ },
-/* 3 */
+/* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -143,19 +294,107 @@
 	  value: true
 	});
 	
-	var _buildElement = __webpack_require__(4);
+	var _buildElement = __webpack_require__(2);
 	
 	var _buildElement2 = _interopRequireDefault(_buildElement);
 	
-	var _dismiss = __webpack_require__(5);
+	var _buildAskElement = __webpack_require__(8);
+	
+	var _buildAskElement2 = _interopRequireDefault(_buildAskElement);
+	
+	var _dismiss = __webpack_require__(3);
 	
 	var _dismiss2 = _interopRequireDefault(_dismiss);
 	
-	var _timer = __webpack_require__(8);
+	var _timer = __webpack_require__(4);
 	
 	var _timer2 = _interopRequireDefault(_timer);
 	
-	var _elementCount = __webpack_require__(6);
+	var _elementCount = __webpack_require__(1);
+	
+	var _elementCount2 = _interopRequireDefault(_elementCount);
+	
+	var _findChild = __webpack_require__(11);
+	
+	var _findChild2 = _interopRequireDefault(_findChild);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	var Ask = function Ask(color, message, title, options, bulletinOuter) {
+	  _elementCount2.default.increase();
+	
+	  var bulletinElement = (0, _buildElement2.default)(color, message, title);
+	  bulletinElement.className += ' bulletin-ask';
+	
+	  var askElements = (0, _buildAskElement2.default)();
+	  bulletinElement.appendChild(askElements);
+	  bulletinOuter.appendChild(bulletinElement);
+	
+	  if (typeof options.onConfirm == 'function') {
+	    var confirmBtn = (0, _findChild2.default)(askElements, 'bulletin-confirm');
+	    confirmBtn.addEventListener('click', options.onConfirm, true);
+	  }
+	};
+	
+	exports.default = Ask;
+
+/***/ },
+/* 8 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var BuildAskElement = function BuildAskElement() {
+	  var askContainer = document.createElement('div');
+	  askContainer.className = 'bulletin-ask-container';
+	
+	  var confirm = document.createElement('div');
+	  confirm.className = 'bulletin-confirm';
+	
+	  var deny = document.createElement('div');
+	  deny.className = 'bulletin-deny';
+	
+	  // TODO: parameterize these values
+	  var confirmText = document.createTextNode('confirm');
+	  var denyText = document.createTextNode('deny');
+	
+	  confirm.appendChild(confirmText);
+	  deny.appendChild(denyText);
+	
+	  askContainer.appendChild(confirm);
+	  askContainer.appendChild(deny);
+	
+	  return askContainer;
+	};
+	
+	exports.default = BuildAskElement;
+
+/***/ },
+/* 9 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _buildElement = __webpack_require__(2);
+	
+	var _buildElement2 = _interopRequireDefault(_buildElement);
+	
+	var _dismiss = __webpack_require__(3);
+	
+	var _dismiss2 = _interopRequireDefault(_dismiss);
+	
+	var _timer = __webpack_require__(4);
+	
+	var _timer2 = _interopRequireDefault(_timer);
+	
+	var _elementCount = __webpack_require__(1);
 	
 	var _elementCount2 = _interopRequireDefault(_elementCount);
 	
@@ -190,122 +429,7 @@
 	exports.default = Notify;
 
 /***/ },
-/* 4 */
-/***/ function(module, exports) {
-
-	'use strict';
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	var BuildElement = function BuildElement(color, message, title) {
-	
-	  var bulletinInner = document.createElement('div');
-	  bulletinInner.className = 'bulletin-inner';
-	
-	  var titleElem = document.createElement('div');
-	  titleElem.className = 'bulletin-title';
-	
-	  var messageElem = document.createElement('div');
-	  messageElem.className = 'bulletin-message';
-	
-	  var titleText = document.createTextNode(title);
-	  var messageText = document.createTextNode(message);
-	
-	  titleElem.appendChild(titleText);
-	  messageElem.appendChild(messageText);
-	
-	  bulletinInner.appendChild(titleElem);
-	  bulletinInner.appendChild(messageElem);
-	
-	  return bulletinInner;
-	};
-	
-	exports.default = BuildElement;
-
-/***/ },
-/* 5 */
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	
-	var _elementCount = __webpack_require__(6);
-	
-	var _elementCount2 = _interopRequireDefault(_elementCount);
-	
-	var _fadeOut = __webpack_require__(7);
-	
-	var _fadeOut2 = _interopRequireDefault(_fadeOut);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	var Dismiss = function Dismiss(elem, bulletinOuter) {
-	  function domRemoval() {
-	    bulletinOuter.removeChild(elem);
-	
-	    var count = _elementCount2.default.decrease();
-	    if (count === 0) {
-	      var body = document.getElementsByTagName('body')[0];
-	      body.removeChild(bulletinOuter);
-	    }
-	  }
-	
-	  (0, _fadeOut2.default)(elem, domRemoval);
-	};
-	
-	exports.default = Dismiss;
-
-/***/ },
-/* 6 */
-/***/ function(module, exports) {
-
-	'use strict';
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	var ElementCount = (function () {
-	
-	  var count = 0;
-	
-	  function getElementCount() {
-	    return count;
-	  }
-	
-	  function decreaseElementCount() {
-	    if (!count) {
-	      count = 0;
-	      return count;
-	    }
-	
-	    count = count - 1;
-	    return count;
-	  }
-	
-	  function increaseElementCount() {
-	    if (!count) {
-	      count = 0;
-	    }
-	
-	    count = count + 1;
-	    return count;
-	  }
-	
-	  return {
-	    get: getElementCount,
-	    increase: increaseElementCount,
-	    decrease: decreaseElementCount
-	  };
-	})();
-	
-	exports.default = ElementCount;
-
-/***/ },
-/* 7 */
+/* 10 */
 /***/ function(module, exports) {
 
 	'use strict';
@@ -328,137 +452,6 @@
 	};
 	
 	exports.default = FadeOut;
-
-/***/ },
-/* 8 */
-/***/ function(module, exports) {
-
-	'use strict';
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	var Timer = function Timer(callback, delay) {
-	  //http://stackoverflow.com/questions/3969475/javascript-pause-settimeout
-	
-	  var timerId,
-	      start,
-	      remaining = delay;
-	
-	  this.pause = function () {
-	    window.clearTimeout(timerId);
-	    remaining -= new Date() - start;
-	  };
-	
-	  this.resume = function () {
-	    start = new Date();
-	    window.clearTimeout(timerId);
-	    timerId = window.setTimeout(callback, remaining);
-	  };
-	
-	  this.clear = function () {
-	    window.clearTimeout(timerId);
-	  };
-	
-	  this.resume();
-	};
-	
-	exports.default = Timer;
-
-/***/ },
-/* 9 */
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	
-	var _buildElement = __webpack_require__(4);
-	
-	var _buildElement2 = _interopRequireDefault(_buildElement);
-	
-	var _buildAskElement = __webpack_require__(10);
-	
-	var _buildAskElement2 = _interopRequireDefault(_buildAskElement);
-	
-	var _dismiss = __webpack_require__(5);
-	
-	var _dismiss2 = _interopRequireDefault(_dismiss);
-	
-	var _timer = __webpack_require__(8);
-	
-	var _timer2 = _interopRequireDefault(_timer);
-	
-	var _elementCount = __webpack_require__(6);
-	
-	var _elementCount2 = _interopRequireDefault(_elementCount);
-	
-	var _findChild = __webpack_require__(11);
-	
-	var _findChild2 = _interopRequireDefault(_findChild);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	var Ask = function Ask(color, message, title, options, bulletinOuter) {
-	  _elementCount2.default.increase();
-	
-	  var bulletinElement = (0, _buildElement2.default)(color, message, title);
-	  bulletinElement.className += ' bulletin-ask';
-	
-	  var askElements = (0, _buildAskElement2.default)();
-	  bulletinElement.appendChild(askElements);
-	  bulletinOuter.appendChild(bulletinElement);
-	
-	  // askElements.id = ElementCount.get();
-	
-	  // var duration = parseInt(options.duration);
-	
-	  if (typeof options.onConfirm == 'function') {
-	    var confirmBtn = (0, _findChild2.default)(askElements, 'bulletin-confirm');
-	    // var confirmBtn = bulletinElement.querySelector('.bulletin-confirm');
-	    confirmBtn.addEventListener('click', (function (e) {
-	      options.onConfirm();
-	    }).bind(this), true);
-	  }
-	};
-	
-	exports.default = Ask;
-
-/***/ },
-/* 10 */
-/***/ function(module, exports) {
-
-	'use strict';
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	var BuildAskElement = function BuildAskElement() {
-	  var askContainer = document.createElement('div');
-	  askContainer.className = 'bulletin-ask-container';
-	
-	  var confirm = document.createElement('div');
-	  confirm.className = 'bulletin-confirm';
-	
-	  var deny = document.createElement('div');
-	  deny.className = 'bulletin-deny';
-	
-	  // TODO: parameterize these values
-	  var confirmText = document.createTextNode('confirm');
-	  var denyText = document.createTextNode('deny');
-	
-	  confirm.appendChild(confirmText);
-	  deny.appendChild(denyText);
-	
-	  askContainer.appendChild(confirm);
-	  askContainer.appendChild(deny);
-	
-	  return askContainer;
-	};
-	
-	exports.default = BuildAskElement;
 
 /***/ },
 /* 11 */
